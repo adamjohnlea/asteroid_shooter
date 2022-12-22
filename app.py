1e293855e523e1e9767e56d070a676c06416880b
adamjohnlea <imnogeekdptcom@me.com>
2022-12-22 01:01:20 -0700

    add iamges and text onto display_surface

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("-= ASTEROID SHOOTER =-")
 
-# importing images. images are also Surfaces
+# Importing images. Images are also Surfaces
+ship_surf = pygame.image.load('./graphics/ship.png').convert_alpha()
+background_surf = pygame.image.load('./graphics/background.png').convert()
+
+# Create text. Text is also a Surface
+font = pygame.font.Font('./graphics/subatomic.ttf', 50)
+text_surf = font.render('Score: ', True, 'white')
 
 # Game Loop
 while True:
@@ -19,6 +25,9 @@ while True:
     # 2 - Updates
     display_surface.fill('black')
     # blit always places top/left of a surface. The top/left corner of the display_surface is (0,0)
+    display_surface.blit(background_surf, (0, 0))
+    display_surface.blit(ship_surf, (300, 500))
+    display_surface.blit(text_surf, (WINDOW_WIDTH - (text_surf.get_width()+120), 20))
 
     # 3 - Update display surface
     pygame.display.update()
