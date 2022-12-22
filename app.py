59e4dd8a8473783dd7562b9bb2d2b8bb1fa0fa52
adamjohnlea <imnogeekdptcom@me.com>
2022-12-22 00:17:53 -0700

    add a Surface, color it, blit it)

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("-= ASTEROID SHOOTER =-")
 
+# Create a surface
+test_surf = pygame.Surface((400, 100))
+# We need to attach the test_surf to the display_surface (done in game loop Updates)
+# We need to make the test_surf a color other than black
+
 # Game Loop
 while True:  # run forever -> keeps our game going
     # 1 - Input -> events (mouse click, button press, etc)
@@ -15,6 +20,9 @@ while True:  # run forever -> keeps our game going
             sys.exit()
 
     # 2 - Updates
+    # display_surface.fill('blue')
+    display_surface.blit(test_surf, (0, 0))
+    test_surf.fill('red')
 
     # 3 - Update display surface
     pygame.display.update()
