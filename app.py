ad8691ba23fa0ee3f01ecd7077c26126f9f9fc7f
adamjohnlea <imnogeekdptcom@me.com>
2022-12-22 00:41:16 -0700

    add graphics and sounds files

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("-= ASTEROID SHOOTER =-")
 
-test_surf = pygame.Surface((400, 100))
+# importing images. images are also Surfaces
 
 # Game Loop
-while True:  # run forever -> keeps our game going
+while True:
     # 1 - Input -> events (mouse click, button press, etc)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -17,9 +17,8 @@ while True:  # run forever -> keeps our game going
             sys.exit()
 
     # 2 - Updates
-    # blit always places top/left of a surface (test_surf), below its placed in top/left (0,0) of the display_surface
-    display_surface.blit(test_surf, (WINDOW_WIDTH - test_surf.get_width(), 0))
-    test_surf.fill('red')
+    display_surface.fill('black')
+    # blit always places top/left of a surface. The top/left corner of the display_surface is (0,0)
 
     # 3 - Update display surface
     pygame.display.update()
